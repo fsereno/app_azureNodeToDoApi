b38873a67c5389aaa1f846ee212814a784edf3a6
Fabio Sereno <fabio_sereno@hotmail.com>
2021-03-17 18:34:19 +0000

    Lets start getting a dummy output

--- Project/listToDos/index.js
+++ Project/listToDos/index.js
@@ -1,13 +1,21 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    return {
+        body: [
+            {
+               "id":"8",
+               "username":"in28minutes",
+               "description":"Become AWS Certified",
+               "targetDate":"2030-07-15T05:46:47.649+0000",
+               "done":false
+            },
+            {
+               "id":"9",
+               "username":"in28minutes",
+               "description":"Learn DevOps with Docker",
+               "targetDate":"2030-07-15T05:47:05.906+0000",
+               "done":false
+            }
+         ]
+    }
 }
