6f265b6cb0648703418ce8a486b3ff80c20728fd
Fabio Sereno <fabio_sereno@hotmail.com>
2021-03-18 16:00:28 +0000

    Added npm package and connecting to mongo db

--- Project/listToDos/index.js
+++ Project/listToDos/index.js
@@ -1,21 +1,17 @@
+const MongoClient = require('mongodb').MongoClient;
+
 module.exports = async function (context, req) {
 
+   const URL = "mongodb://cosmos-db-fsereno:cK7M8N98HicntPkPwT7RJg1KLFTfyBzKfUISZ7jVFR0yBc678hvKRYTlMv4bn8HFR8KjvRkHt5YJbiv13tdN4Q%3D%3D@cosmos-db-fsereno.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-fsereno@";
+   const DATABASE_NAME = "cosmos-db-fsereno";
+   const COLLECTION_NAME = "todos";
+
+   const connection = await MongoClient.connect(URL);
+   const todoCollection = connection.db(DATABASE_NAME).collection(COLLECTION_NAME);
+
+   const result = await todoCollection.find({}).toArray();
+
     return {
-        body: [
-            {
-               "id":"8",
-               "username":"in28minutes",
-               "description":"Become AWS Certified",
-               "targetDate":"2030-07-15T05:46:47.649+0000",
-               "done":false
-            },
-            {
-               "id":"9",
-               "username":"in28minutes",
-               "description":"Learn DevOps with Docker",
-               "targetDate":"2030-07-15T05:47:05.906+0000",
-               "done":false
-            }
-         ]
+        body: JSON.stringify(result)
     }
 }
