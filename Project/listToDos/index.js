f5f20096816d31e640da58b084f18c9441fc6ab4
Fabio Sereno <fabio_sereno@hotmail.com>
2021-03-18 16:46:42 +0000

    Moving to env vars

--- Project/listToDos/index.js
+++ Project/listToDos/index.js
@@ -2,9 +2,9 @@ const MongoClient = require('mongodb').MongoClient;
 
 module.exports = async function (context, req) {
 
-   const URL = "mongodb://cosmos-db-fsereno:cK7M8N98HicntPkPwT7RJg1KLFTfyBzKfUISZ7jVFR0yBc678hvKRYTlMv4bn8HFR8KjvRkHt5YJbiv13tdN4Q%3D%3D@cosmos-db-fsereno.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-fsereno@";
-   const DATABASE_NAME = "cosmos-db-fsereno";
-   const COLLECTION_NAME = "todos";
+   const URL = process.env.MONGODB_URL;
+   const DATABASE_NAME = process.env.MONGODB_DATABASE_NAME;
+   const COLLECTION_NAME = process.env.MONGODB_COLLECTION_NAME;
 
    const connection = await MongoClient.connect(URL);
    const todoCollection = connection.db(DATABASE_NAME).collection(COLLECTION_NAME);
@@ -12,6 +12,6 @@ module.exports = async function (context, req) {
    const result = await todoCollection.find({}).toArray();
 
     return {
-        body: JSON.stringify(result)
+        body: JSON.stringify(result).replace(/_id/g, "id")
     }
 }
